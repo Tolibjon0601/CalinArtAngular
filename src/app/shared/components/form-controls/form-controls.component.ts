c8686f59b832a9559a9e35a19e7b386d642a666d
Tolibjon0601 <egamberdiyevtolibjon0601@gmail.com>
2025-06-17 19:22:27 +0500

    start

---
+++ src/app/shared/components/form-controls/form-controls.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-form-controls',
+  imports: [],
+  templateUrl: './form-controls.component.html',
+  styleUrl: './form-controls.component.scss'
+})
+export class FormControlsComponent {
+
+}
